35ec9304cc2b1a2c7f142c38d1e4a3b9d5293078
antmmalone <antmmalone@gmail.com>
2020-04-26 22:34:46 -0400

    Yaml time part 5: That goofy one

--- .github/test_tasks.py
+++ .github/test_tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import pytest, os
-os.chdir("/home/runner/work/Pfsense-Vpn-Refresh/Pfsense-Vpn-Refresh")
+import pytest,os
+os.chdir("../")
 from tasks import *
 
 def test_functions():
